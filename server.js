e43f028d3e28cc4f7d68b71435a2eede420013f8
Chuan <2209339564@qq.com>
2024-07-31 12:44:51 +0800

    update initial version

--- server.js
+++ server.js
@@ -1,42 +1,23 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-//
-// const login = require('./apps/login');
-// const house = require('./apps/house');
-// const items = require('./apps/items');
-// const util = require('./lib/util');
-// //
 
+// local modules
+const temp_monitor = require('./apps/temp_monitor');
 
+// setup express
 const app = express();
-
 let intialPath = path.join(__dirname, "public");
-console.log(intialPath);
-
 app.use(bodyParser.json());
 app.use(express.static(intialPath));
 
 // Router
 app.get('/', (req, res) => { res.sendFile(path.join(intialPath, "index.html")); })
-// app.get('/login', (req, res) => { res.sendFile(path.join(intialPath, "login.html")); })
-// app.get('/register', (req, res) => { res.sendFile(path.join(intialPath, "register.html")); })
-// app.get('/house', (req, res) => { res.sendFile(path.join(intialPath, "house.html")); })
-// app.get('/room', (req, res) => { res.sendFile(path.join(intialPath, "room.html")); })
-
-
+// setInterval(temp_monitor.updateTempDB_, 10000); // move this to monitor.js
 app.listen(3000, (req, res) => {
     console.log('listening on port 3000......')
 })
 
+app.post('/temp-get', temp_monitor.getDBData_);
 
-// app.post('/register-user', login.register_user)
-// app.post('/login-user', login.login_user)
-// //
-// app.post('/house', house.create_house)
-// app.post('/house-get', house.get_houses)
-// //
-// app.post('/get-items', items.get_items)
-// app.post('/add-items', items.add_items)
-// app.post('/del-items', items.del_items)
-// app.post('/update-items', items.update_items)
+// nohup node server.js > log.txt &
